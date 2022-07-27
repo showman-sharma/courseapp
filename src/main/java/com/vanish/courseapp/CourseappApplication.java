bba01af2d767e4b9298e7f0edd865c49063bfe03
Anirudh Sharma <60467702+showman-sharma@users.noreply.github.com>
2022-07-27 14:02:25 +0530

    Removed unnecessary lines

--- src/main/java/com/vanish/courseapp/CourseappApplication.java
+++ src/main/java/com/vanish/courseapp/CourseappApplication.java
@@ -4,8 +4,6 @@ import com.vanish.courseapp.configuration.beanConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication(scanBasePackages = {"com.vanish.courseapp.*"},exclude={DataSourceAutoConfiguration.class})
 public class CourseappApplication {
@@ -13,8 +11,6 @@ public class CourseappApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(CourseappApplication.class, args);
-		ApplicationContext context = new AnnotationConfigApplicationContext(beanConfig.class);
-//		Vehicle vehicle = context.getBean(Vehicle.class);
 
 	}
 
